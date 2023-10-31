a5097e3bf60acbde36a4f982bfdac02bbc5c22f8
Jean Berin <51207250+PiLord1@users.noreply.github.com>
2023-10-31 23:42:25 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#fa-bars');
-let navbar = document.querySelector('top-nav-middle-repositioned');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
-}
