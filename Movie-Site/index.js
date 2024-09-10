200c9f2617062e174edc49034639de07aacee215
Jean Berin <51207250+PiLord1@users.noreply.github.com>
2024-09-10 20:59:26 +0800

    Add files via upload

--- Movie-Site/index.js
+++ Movie-Site/index.js
@@ -1,56 +1,43 @@
 const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a6d59a26b14b3e6f6faa67b19b86b9c9&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=a6d59a26b14b3e6f6faa67b19b86b9c9&query="';
-
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=a6d59a26b14b3e6f6faa67b19b86b9c9&query=';
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
 const search = document.getElementById("query");
 
-returnMovies(APILINK)
+returnMovies(APILINK);
 
 function returnMovies(url) {
-    fetch(url).then(res => res.json())
-    .then(function(data){
-        console.log(data.results);
-        data.results.forEach(element => {
-            const div_card = document.createElement('div');
-            div_card.setAttribute('class', 'card');
+    fetch(url)
+        .then(res => res.json())
+        .then(function (data) {
+            main.innerHTML = ''; 
+            data.results.forEach(element => {
+                const div_card = document.createElement('div');
+                div_card.setAttribute('class', 'card');
 
-            const div_column = document.createElement('div');
-            div_column.setAttribute('class', 'column');
+                const image = document.createElement('img');
+                image.setAttribute('class', 'thumbnail');
+                image.src = IMG_PATH + element.poster_path;
 
-            const div_row = document.createElement('div');
-            div_row.setAttribute('class', 'row');
+                const title = document.createElement('h3');
+                title.innerHTML = element.title;
 
-            const image = document.createElement('img');
-            image.setAttribute('class', 'thumbnail');
-            image.setAttribute('id', 'image');
+                div_card.appendChild(image);
+                div_card.appendChild(title);
 
-            const title = document.createElement('h3');
-            title.setAttribute('id', 'title');
-
-            title.innerHTML = `${element.title}`;
-            image.src = IMG_PATH + element.poster_path;
-
-            div_card.appendChild(image);
-            div_card.appendChild(title);
-            div_column.appendChild(div_card);
-            div_row.appendChild(div_column);
-
-            main.appendChild(div_row);
+                main.appendChild(div_card);
+            });
         });
-    });
 }
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    main.innerHTML = '';
-
     const searchItem = search.value;
 
     if (searchItem) {
         returnMovies(SEARCHAPI + searchItem);
         search.value = "";
-    } 
-})
+    }
+});
